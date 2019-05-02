8762ad142238c7fbcba288aed58cb1230714cbd8
Diego Magdaleno <dmmagdal@ucsc.edu>
2019-05-02 16:43:07 -0700

    debug for auth0

--- App.js
+++ App.js
@@ -33,7 +33,7 @@ const MainNavigator = createStackNavigator(
     AccumulationSweep: {screen: AccumulationSweep}
   },
   {
-    initialRouteName: 'Area'
+    initialRouteName: 'Boarding'
   }
 );
 
