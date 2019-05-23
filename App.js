496e3b3e0d6bcc23520263d322f68432361f23b1
Diego Magdaleno <dmmagdal@ucsc.edu>
2019-05-22 18:39:17 -0700

    commented and made small changes to app.js.

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-
 import {createStackNavigator, createNavigationContainer, createAppContainer} from 'react-navigation';
-
 import { StyleSheet, Text, View, Button } from 'react-native';
 import {Root} from 'native-base'
 
@@ -9,36 +7,24 @@ import {Root} from 'native-base'
 import { useScreens } from 'react-native-screens';
 useScreens();
 
+// Screens
 import HomePage from './screens/HomePage';
 import BoardingPage from './screens/BoardingPage';
 import LogInPage from './screens/LogInPage';
-import ProfilePage from './screens/ProfilePage';
 import SurveyPage from './screens/SurveyPage';
 import PublishContainer from './screens/Publish/PublishContainer';
-import ChooseBeachPage from './screens/ChooseBeachPage';
-import TeamInfo from './screens/survey/TeamInfo';
-import Area from './screens/survey/Area';
-import SurfaceRibScan from './screens/survey/SurfaceRibScan';
-import AccumulationSweep from './screens/survey/AccumulationSweep';
-import MicroDebris from './screens/survey/MicroDebris';
-import Scanner from './screens/Publish/Scanner';
 import SurveyContainer from './screens/survey/SurveyContainer'
-import Example from './screens/Example';
 
 
-
+// Navigator to different screens/pages.
 const MainNavigator = createStackNavigator(
   { 
     Boarding: {screen: BoardingPage},
     Home: {screen: HomePage},
     Login: {screen: LogInPage},
-    Profile: {screen: ProfilePage},
     SurveyEntry: {screen: SurveyPage},
     PublishContainer: {screen: PublishContainer},
-    ChooseBeach: {screen: ChooseBeachPage},
     SurveyContainer: {screen: SurveyContainer},
-    Example : {screen: Example}
-
   },
   {
     // First init route is for testing, second init route is for published app
@@ -47,14 +33,13 @@ const MainNavigator = createStackNavigator(
   }
 );
 
+// Create the appcontainer that will navigate between screens.
 const NavigationApp = createAppContainer(MainNavigator);
-
-//export default App;
 
 
 export default class App extends React.Component {
 
-
+  // Render the items in the class. (DO NOT CHANGE).
   render() {
     return (
       <Root>
@@ -63,15 +48,3 @@ export default class App extends React.Component {
     );
   }
 }
-
-
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
