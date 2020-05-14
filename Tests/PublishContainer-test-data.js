34caacc5c3859631242e5b4fded975ad16b1b3d4
Sean Gibson <stgibson@ucsc.edu>
2020-05-13 17:27:25 -0700

    Created PublishContainer-test-data.js. Created header comment and skeleton code for the two variables to be used for testing calculateTotals(type) in screens/Publish/PublishContainer.js.

---
+++ Tests/PublishContainer-test-data.js
@@ -0,0 +1,14 @@
+/**
+ * PublishContainer-test-data.js
+ * Stores variables to be used by PublishContainer.test.js to test
+ * ../screens/Publish/PublishContainer.js.
+ */
+var testCalculateTotalsMergedSurvey1 = {
+  // TODO
+};
+
+var testCalculateTotalsExpect1 = {
+  // TODO
+};
+
+export { testCalculateTotalsMergedSurvey1, testCalculateTotalsExpect1 };
