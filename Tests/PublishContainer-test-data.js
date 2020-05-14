b039fee043692beef7004b264e37d42debe56429
Sean Gibson <stgibson@ucsc.edu>
2020-05-13 20:45:42 -0700

    Created new vars in PublishContainer-test-data.js. Modified Tests/PublishContainer-test-data.js by creating testCalculateTotalsMDSMergedSurvey2 and testCalculateTotalsMDSExpect2.

--- Tests/PublishContainer-test-data.js
+++ Tests/PublishContainer-test-data.js
@@ -3,7 +3,8 @@
  * Stores variables to be used by PublishContainer.test.js to test
  * ../screens/Publish/PublishContainer.js.
  */
-var testCalculateTotalsMergedSurvey1 = {
+
+var testCalculateTotalsMDSMergedSurvey1 = {
   surveyData: {
     beachName: "Boardwalk Beach",
     cleanupDate: new Date("2020-03-24T07:00:00.000Z"),
@@ -125,8 +126,152 @@ var testCalculateTotalsMergedSurvey1 = {
   }
 };
 
-var testCalculateTotalsExpect1 = {
-  // TODO
+var testCalculateTotalsMDSMergedSurvey2 = {
+  surveyData: {
+    beachName: "Boardwalk Beach",
+    cleanupDate: new Date("2020-03-24T07:00:00.000Z"),
+    cleanupTime: new Date("2020-05-11T17:30:00.000Z"),
+    cmpsDir: "180",
+    latitude: "36",
+    locationChoiceDebris: true,
+    longitude: "-122",
+    orgLoc: "Santa Cruz, United States of America",
+    orgName: "Clean Oceans International",
+    riverDistance: "10",
+    riverName: "Kawa Rivera",
+    slope: "winter",
+    substrateTypeSand: true,
+    tideHeightA: "10",
+    tideHeightB: "10",
+    tideTimeA: new Date("2020-05-11T23:16:45.762Z"),
+    tideTimeB: new Date("2020-05-11T23:05:32.726Z"),
+    tideTypeA: "low",
+    tideTypeB: "high",
+    usageRecreation: true,
+    userFirst: "Sean",
+    userLast: "Gibson",
+    windDir: "s",
+    windSpeed: "10"
+  },
+  SRSData: {
+    cigaretteButts__fresh__1: 5,
+    cigaretteButts__fresh__2: 6,
+    cigaretteButts__fresh__3: 4,
+    cigaretteButts__fresh__4: 5,
+    cigaretteButts__weathered__1: 3,
+    cigaretteButts__weathered__3: 3,
+    cigaretteButts__weathered__4: 3,
+    fishingLineRope__fresh__1: 1,
+    fishingLineRope__fresh__2: 2,
+    fishingLineRope__fresh__3: 4,
+    fishingLineRope__fresh__4: 3,
+    fishingLineRope__weathered__1: 3,
+    fishingLineRope__weathered__2: 4,
+    plasticStraws__fresh__3: 1,
+    plasticStraws__fresh__4: 3,
+    plasticStraws__weathered__1: 1,
+    plasticStraws__weathered__2: 6,
+    plasticStraws__weathered__3: 3,
+    plasticStraws__weathered__4: 2,
+    plasticBottlesCaps__fresh__1: 1,
+    plasticBottlesCaps__fresh__2: 1,
+    plasticBottlesCaps__fresh__3: 1,
+    plasticBottlesCaps__weathered__1: 2,
+    plasticBottlesCaps__weathered__2: 4,
+    plasticBottlesCaps__weathered__3: 3,
+    plasticBottlesCaps__weathered__4: 1,
+    aluminumCans__fresh__1: 1,
+    aluminumCans__fresh__4: 1,
+    aluminumCans__weathered_3: 1,
+    aluminumCans__weathered_4: 1,
+    glass__fresh__1: 1,
+    glass__fresh__2: 1,
+    styrothane__fresh__1: 1,
+    styrothane__fresh__2: 1,
+    styrothane__fresh__3: 1,
+    styrothane__fresh__4: 1,
+    styrothane__weathered__1: 1,
+    styrothane__weathered__2: 1,
+    styrothane__weathered__3: 1,
+    styrothane__weathered__4: 1,
+    otherPlastic__fresh__1: 2,
+    otherPlastic__fresh__2: 1,
+    otherPlastic__fresh__3: 1,
+    otherPlastic__fresh__4: 2,
+    otherPlastic__weathered__1: 1,
+    otherPlastic__weathered__2: 1,
+    otherPlastic__weathered__4: 1,
+    otherFood__fresh__1: 2,
+    otherFood__fresh__3: 1,
+    otherFood__fresh__4: 2,
+    otherFood__weathered__1: 1,
+    otherCotton__fresh__1: 1,
+    otherCotton__fresh__2: 1,
+    otherCotton__fresh__4: 2,
+    otherCotton__weathered__1: 2,
+    otherCotton__weathered__2: 2,
+    otherCotton__weathered__3: 4,
+    otherWood__fresh__1: 1,
+    otherWood__fresh__2: 1,
+    otherWood__fresh__3: 1,
+    otherWood__fresh__4: 1,
+    otherWood__weathered__1: 1,
+    otherWood__weathered__2: 1,
+    otherWood__weathered__3: 1,
+    otherWood__weathered__4: 1
+  },
+  ASData: {
+    cigaretteButts__weathered__accumulation: 10,
+    fishingLineRope__weathered__accumulation: 2,
+    plasticStraws__weathered__accumulation: 13,
+    filmedPlastic__fresh__accumulation: 2,
+    filmedPlastic__weathered__accumulation: 3,
+    plasticBottlesCaps__fresh__accumulation: 1,
+    aluminumCans__fresh__accumulation: 3,
+    aluminumCans__weathered__accumulation: 6,
+    glass__weathered__accumulation: 2,
+    styrothane__fresh__accumulation: 2,
+    styrothane__weathered__accumulation: 7,
+    otherPlastic__weathered__accumulation: 34,
+    otherFood__weathered__accumulation: 3
+  },
+  MicroData: {
+    rib1__fresh__micro: 1,
+    rib1__weathered__micro: 1,
+    rib2__fresh__micro: 3,
+    rib2__weathered__micro: 2,
+    rib3__fresh__micro: 2,
+    rib3__weathered__micro: 2,
+    rib4__fresh__micro: 1,
+    rib4__weathered__micro: 3
+  },
+  ribData: {
+    r1Length: "50",
+    r1Start: "5",
+    r2Length: "50",
+    r2Start: "10",
+    r3Length: "50",
+    r3Start: "16",
+    r4Length: "50",
+    r4Start: "27"
+  }
 };
 
-export { testCalculateTotalsMergedSurvey1, testCalculateTotalsExpect1 };
+var testCalculateTotalsMDSExpect1 = [];
+
+var testCalculateTotalsMDSExpect2 = [
+  [
+    "microdebris",
+    {
+      fresh: 7,
+      weathered: 8
+    }
+  ]
+];
+
+export {
+  testCalculateTotalsMDSMergedSurvey1,
+  testCalculateTotalsMDSMergedSurvey2,
+  testCalculateTotalsMDSExpect1,
+  testCalculateTotalsMDSExpect2
+};
